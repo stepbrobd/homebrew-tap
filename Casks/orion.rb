92055cbd9ac0c91b4892531b19abc395103668aa
Yifei Sun <ysun@hey.com>
2024-08-05 21:02:31 +0900

    cask: orion fix no '>=' for versions newer than macos 14

---
+++ Casks/orion.rb
@@ -0,0 +1,96 @@
+cask "orion" do
+  version "0.99,128.2"
+
+  on_mojave :or_older do
+    sha256 "e92bd55e164e2eebe817150945791ddd68bf543f4ed6a44340b56511799e88e1"
+
+    url "https://browser.kagi.com/updates/10_14/#{version.csv.second}.zip"
+
+    livecheck do
+      url "https://cdn.kagi.com/updates/10_14/appcast.xml"
+      strategy :sparkle
+    end
+
+    depends_on macos: :mojave
+  end
+  on_catalina do
+    sha256 "bfea849fe58efb5e5594fd1f5acc1b93294c40bd01ab52c533e477578088031b"
+
+    url "https://browser.kagi.com/updates/10_15/#{version.csv.second}.zip"
+
+    livecheck do
+      url "https://cdn.kagi.com/updates/10_15/appcast.xml"
+      strategy :sparkle
+    end
+
+    depends_on macos: :catalina
+  end
+  on_big_sur do
+    sha256 "8880ee6ea9a0be33f306a4ea8f45c22d20c361c553f8ebae49459e99e29fc63c"
+
+    url "https://browser.kagi.com/updates/11_0/#{version.csv.second}.zip"
+
+    livecheck do
+      url "https://cdn.kagi.com/updates/11_0/appcast.xml"
+      strategy :sparkle
+    end
+
+    depends_on macos: :big_sur
+  end
+  on_monterey do
+    sha256 "e183d81bd6926355340f9135dd111679fcca2a571a8b91de1ef9abbcc619eeed"
+
+    url "https://browser.kagi.com/updates/12_0/#{version.csv.second}.zip"
+
+    livecheck do
+      url "https://cdn.kagi.com/updates/12_0/appcast.xml"
+      strategy :sparkle
+    end
+
+    depends_on macos: :monterey
+  end
+  on_ventura do
+    sha256 "60ce57d69ad709cac9fcb381d4845a7c4f355e351f54073b28baec02ab8b84a8"
+
+    url "https://browser.kagi.com/updates/13_0/#{version.csv.second}.zip"
+
+    livecheck do
+      url "https://cdn.kagi.com/updates/13_0/appcast.xml"
+      strategy :sparkle
+    end
+
+    depends_on macos: :ventura
+  end
+  on_sonoma :or_newer do
+    sha256 "c6306e3950e0d1d6465f0b8ec435232fc528e9bccae82e17f80bd718bf469beb"
+
+    url "https://browser.kagi.com/updates/14_0/#{version.csv.second}.zip"
+
+    livecheck do
+      url "https://cdn.kagi.com/updates/14_0/appcast.xml"
+      strategy :sparkle
+    end
+
+    depends_on macos: ">= :sonoma"
+  end
+
+  name "Orion Browser"
+  desc "WebKit based web browser"
+  homepage "https://browser.kagi.com/"
+
+  auto_updates true
+
+  app "Orion.app"
+
+  uninstall quit: "com.kagi.kagimacOS"
+
+  zap trash: [
+    "~/Library/Application Scripts/com.kagi.kagimacOS.ShareExtension",
+    "~/Library/Application Support/Orion",
+    "~/Library/Caches/com.kagi.kagimacOS",
+    "~/Library/Containers/com.kagi.kagimacOS.ShareExtension",
+    "~/Library/HTTPStorages/com.kagi.kagimacOS*",
+    "~/Library/Preferences/com.kagi.kagimacOS.plist",
+    "~/Library/WebKit/com.kagi.kagimacOS",
+  ]
+end
