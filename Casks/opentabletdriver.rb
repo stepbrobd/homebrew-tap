3587092baec3a468706fb32756c4e96c0bb9cbf8
Yifei Sun <ysun@hey.com>
2026-09-05 23:17:38 +0200

    cask: opentabletdriver drop deprecated `verified`

--- Casks/opentabletdriver.rb
+++ Casks/opentabletdriver.rb
@@ -2,8 +2,7 @@ cask "opentabletdriver" do
   version "0.6.7"
   sha256 "c4b0d9eb2aee8ee7be12d0df80f961ea0a7235f54b5f59a2b62efa9b9ec4910e"
 
-  url "https://github.com/OpenTabletDriver/OpenTabletDriver/releases/download/v#{version}/OpenTabletDriver-#{version}_osx-x64.tar.gz",
-      verified: "github.com/OpenTabletDriver/OpenTabletDriver/"
+  url "https://github.com/OpenTabletDriver/OpenTabletDriver/releases/download/v#{version}/OpenTabletDriver-#{version}_osx-x64.tar.gz"
   name "OpenTabletDriver"
   desc "Open source, cross platform, user mode tablet driver"
   homepage "https://opentabletdriver.net/"
